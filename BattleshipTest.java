21bb73789b3265de80d9da2ca756f1fd0c83292a
Hawa Abdi <abdih17@hotmail.com>
2017-06-05 11:36:16 -0700

    completed mini assignment for CS142: added the test class

---
+++ BattleshipTest.java
@@ -0,0 +1,15 @@
+package Battleship;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BattleshipTest {
+
+	@Test
+	public void test() 
+	{
+		fail("Not yet implemented");
+	}
+
+}
